11193de2ad901f66cd8a3516b5450815c31194ed
Ashi Gautam <74711030+Change-m@users.noreply.github.com>
2022-05-22 18:06:53 +0530

    Update #Day1P1.py

--- #Day1P1.py
+++ #Day1P1.py
@@ -33,6 +33,20 @@ class Solution:
         return 0 # triangle largest perameter in nums array if impossible return 0  
     
     
+    
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        (x0, y0), (x1, y1) = coordinates[0], coordinates[1]
+        for i in range(2, len(coordinates)):
+            x, y = coordinates[i]
+            if (x0 - x1) * (y1 - y) != (x1 - x) * (y0 - y1):
+                return False
+        return True  #straight line in the graph
+    
+    
+    
+    
+    
+    
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         n = []
         for num in nums1:
