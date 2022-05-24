7b644142ecbab53368c7ce3d34d342ed45af308b
Ashi Gautam <74711030+Change-m@users.noreply.github.com>
2022-05-24 16:59:42 +0530

    Update #Day1P1.py

--- #Day1P1.py
+++ #Day1P1.py
@@ -120,5 +120,18 @@ class Solution:
         return ans #take odd subarray of the list and add all the numbers
     
     
+    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+        ans = 0
+        for i in range (len(arr)+1):
+            for j in range(i):
+                if len(arr[j:i])%2!=0:
+                    ans+=sum(arr[j:i])
+        return ans
+    
+    
+    
+    
+    
+    
     
     
