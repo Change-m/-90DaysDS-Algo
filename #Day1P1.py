baf818fce226049bb4dd2daa1b8d68cd2105e0d1
Ashi Gautam <74711030+Change-m@users.noreply.github.com>
2022-05-28 23:21:57 +0530

    Update #Day1P1.py

--- #Day1P1.py
+++ #Day1P1.py
@@ -277,8 +277,21 @@ class Solution:
         return dfs(root, 0)
     
     
-    
-    
+     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        res=0
+        q=[(root,0)]
+        
+        while q:
+            node,l=q.pop(0)
+            if (not node.left) and (not node.right) and l:
+                res+=node.val
+            if node.left:
+                q.append((node.left,1))
+            if node.right:
+                q.append((node.right,0))
+        
+        return res
+     
     
     
     
