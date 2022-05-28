238597f2587732354c198419ee7c003313819390
Ashi Gautam <74711030+Change-m@users.noreply.github.com>
2022-05-28 23:08:51 +0530

    Update #Day1P1.py

--- #Day1P1.py
+++ #Day1P1.py
@@ -268,4 +268,17 @@ class Solution:
         return s   
         
         
-        
+     
+     def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def dfs(root, depth):
+            if not root: return depth
+            return max(dfs(root.left, depth + 1), dfs(root.right, depth + 1))
+                       
+        return dfs(root, 0)
+    
+    
+    
+    
+    
+    
+    
