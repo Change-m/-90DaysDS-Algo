f5dcd8d8d368a5cda7a685e37ac2f6fde0c6ce72
Ashi Gautam <74711030+Change-m@users.noreply.github.com>
2022-05-31 20:38:00 +0530

    Update #DailyPractice.py

--- #DailyPractice.py
+++ #DailyPractice.py
@@ -11,3 +11,13 @@ Example 2:
 Input: dividend = 7, divisor = -3
 Output: -2
 Explanation: 7/-3 = -2.33333.. which is truncated to -2.'''
+
+        
+        
+def hasAllCodes(self, s: str, k: int) -> bool:
+        return len({s[i:i+k] for i in range(len(s)-k+1)}) == 2 ** k
+'''Example 1:
+
+Input: s = "00110110", k = 2
+Output: true
+Explanation: The binary codes of length 2 are "00", "01", "10" and "11". They can be all found as substrings at indices 0, 1, 3 and 2 respectively.'''
