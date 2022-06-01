9781c7dce0e9865a28135fdd5567c6817460d890
Ashi Gautam <74711030+Change-m@users.noreply.github.com>
2022-06-01 16:39:01 +0530

    Update #1DayAlgoPractice.py

--- #1DayAlgoPractice.py
+++ #1DayAlgoPractice.py
@@ -84,3 +84,17 @@ def rotate(self, nums: List[int], k: int) -> None:
         nums[:] = nums[-k:] + nums[:-k]        
         
         
+def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l,r=0,len(numbers)-1
+        while l<r:
+            if numbers[l]+numbers[r] < target :
+                l+=1
+            elif numbers[l]+numbers[r] > target :
+                r-=1
+            else :
+                return [l+1,r+1]
+            '''Example 1:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].'''
