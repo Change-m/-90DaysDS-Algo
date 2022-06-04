d8530335e436ba4c6e43064d0f7d11feab68b16f
Ashi Gautam <74711030+Change-m@users.noreply.github.com>
2022-06-04 17:48:50 +0530

    Update #1DayAlgoPractice.py

--- #1DayAlgoPractice.py
+++ #1DayAlgoPractice.py
@@ -177,8 +177,41 @@ def lengthOfLongestSubstring(self, s: str) -> int:
             longest = max(longest, right_p - left_p + 1)
 
         return longest
+    
+def lengthOfLongestSubstring(self, s: str) -> int:
+        ans = 0
+        for i in range(len(s)):
+            st = set()
+            for j in range(i, len(s)):
+                if s[j] in st:
+                    break
+                st.add(s[j])
+                ans = max(ans, len(st))
+ 
+        return ans
     '''Example 1:
 
 Input: s = "abcabcbb"
 Output: 3
 Explanation: The answer is "abc", with the length of 3.'''
+
+def checkInclusion(self, s1: str, s2: str) -> bool
+    if len(s1) > len(s2):
+        return False
+    length_of_s1 = len(s1)
+    s1_counter = Counter(s1)
+    window_counter = Counter()
+    for i, c in enumerate(s2):
+        window_counter[c] += 1
+        if i >= length_of_s1:
+            element_from_left = s2[i - length_of_s1]
+            if window_counter[element_from_left] == 1:
+                del window_counter[element_from_left]
+            else:
+                window_counter[element_from_left] -= 1
+         if window_counter == s1_counter:
+              return True
+      return False
+                
+
+
